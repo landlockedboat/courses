75ddd2189baa5b5e481dd25fe4d62a3e91612f8e
vikepic <victorenriquealcazar@gmail.com>
2020-01-16 14:17:26 +0100

    :sparkles: Added recipes to recipe page

--- ionic-angular-course/ionic-angular-course/src/app/recipes/recipes.page.ts
+++ ionic-angular-course/ionic-angular-course/src/app/recipes/recipes.page.ts
@@ -1,11 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.page.html',
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+
+  recipes: Recipe[] = [
+    {
+      id: 'r1',
+      title: 'Schnitzel',
+      imageUrl: 'https://loremflickr.com/640/360',
+      ingredients: ['French Fries', 'Tofu', 'Salad']
+    },
+    {
+      id: 'r2',
+      title: 'Spaghetti',
+      imageUrl: 'https://loremflickr.com/640/360',
+      ingredients: ['Spaghetti', 'Vegan cheese', 'Tomatico']
+    }
+  ];
 
   constructor() { }
 
